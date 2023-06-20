86261f140e9020ef885c6d178a0afd3943661c6f
NeverNotJohn <johntheherrera@gmail.com>
2023-06-20 11:23:11 -0400

    finish class formatting

--- brainy/brainy.py
+++ brainy/brainy.py
@@ -3,21 +3,26 @@
 ########################
 
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds, BrainFlowPresets
+from func import update
+
 
 class brainy:
     
-    def __init__(self, board, COM_Num):
+    def __init__(self, board, COM_Num, file=""):
         
         """ Logic: """
         
-        params = BrainFlowInputParams()                                             # Params I guess 
-        params.serial_port = f"COM{COM_Num}"                                        # Only Param needed                
+        params = BrainFlowInputParams()                                             # Params I guess       
         
         if (board == "cyton"):
+            params.serial_port = f"COM{COM_Num}"                                    # Only Param needed          
             board_input = BoardShim(BoardIds.CYTON_DAISY_BOARD, params)
         elif (board == "virtual"):
+            params.master_board = BoardIds.SYNTHETIC_BOARD
+            params.file = file                                                      # Path to recording file
             board_input = BoardShim(BoardIds.PLAYBACK_FILE_BOARD, params)
         elif (board == "ganglion"):
+            params.serial_port = f"COM{COM_Num}"                                    # Only Param needed          
             board_input = BoardShim(BoardIds.GANGLION_BOARD, params)
         
         """ Variables: """ 
@@ -25,8 +30,13 @@ class brainy:
         self.board = board_input
         self.COM = f"COM{COM_Num}"
         
-        """ Functions """
+        """ Start """
         
+        board.prepare_session()
+        board.start_stream()
+
+    """ Functions: """
+
+    def update(self):   # FIXME
+        update.refresh(self.board)
         
-        
-        # Functions:
--- tests/test.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-array1 = [1, 2, 3, 4]
-array2 = [1, 2, 3, 4]
-
-plt.plot(array1, array2)
-
-plt.show()
