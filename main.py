6fc4e90280e7eb40e30b03d08adb74308d9b9f01
NeverNotJohn <johntheherrera@gmail.com>
2023-07-20 21:24:51 -0400

    added board setting

--- main.py
+++ main.py
@@ -1,46 +1,39 @@
-import dearpygui.dearpygui as dpg
 from brainy import gui
+import dearpygui.dearpygui as dpg
 
-def main():
-    
-    """ INITIALIZE GUI """
-    
-    dpg.create_context()
-    dpg.create_viewport(title='Brainy Computer Interface', width=500, height=500)
 
-    with dpg.window(label="main", tag="Primary Window", no_title_bar=True, no_collapse=True, no_move=True):
+""" INITIALIZE GUI """
+dpg.create_context()
+dpg.create_viewport(title='Brainy Computer Interface', width=500, height=500)
+
+
+
+""" MAIN """
+
+with dpg.window(label="main", tag="Primary Window", no_title_bar=True, no_collapse=True, no_move=True):
+    
+    dpg.add_text("Settings:")
+    
+    """ Settings """
+    
+    # Selecting Board
+    dpg.add_listbox(label="Select Board", items=gui.boardList, width=80, callback=gui.getBoard)
         
-        dpg.add_text("Settings:")
-        
-        """ Settings """
-        
-            # Create the dropdown
-        with dpg.combo(label="Select Board", width=200, callback=dropdown_callback):
-
-            #Drop down options
-            
-            dpg.add_selectable(label="Option 1", callback=dropdown_callback)
-            dpg.add_selectable(label="Option 2", callback=dropdown_callback)
-            dpg.add_selectable(label="Option 3", callback=dropdown_callback)
-
-        
-        
-
-
-
-    """ RUN GUI """
-
-    dpg.setup_dearpygui()
-    dpg.show_viewport()
-    dpg.set_primary_window("Primary Window", True)
-    dpg.start_dearpygui()
-    dpg.destroy_context()
     
-    
-    
-    """ Debug """
-    
-    # createMP3("Test", 200, 1.0, 1)
 
-if __name__ == "__main__":
-    main()
+
+
+""" RUN GUI """
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.set_primary_window("Primary Window", True)
+dpg.start_dearpygui()
+dpg.destroy_context()
+
+
+
+""" Debug """
+
+# createMP3("Test", 200, 1.0, 1)
+
