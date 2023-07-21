a8ee9c14878aa0c0b5011c84e29e524cef040042
NeverNotJohn <johntheherrera@gmail.com>
2023-07-20 20:59:05 -0400

    changing structure

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import dearpygui.dearpygui as dpg
+from brainy import gui
 
 def main():
+    
+    """ INITIALIZE GUI """
     
     dpg.create_context()
     dpg.create_viewport(title='Brainy Computer Interface', width=500, height=500)
@@ -9,12 +12,23 @@ def main():
         
         dpg.add_text("Settings:")
         
-        dpg.add_input_text(label="Input String", default_value="Quick brown fox")
-        dpg.add_slider_float(label="Volume", default_value=50, max_value=100)
+        """ Settings """
         
-        dpg.add_button(label="Run!")
+            # Create the dropdown
+        with dpg.combo(label="Select Board", width=200, callback=dropdown_callback):
+
+            #Drop down options
+            
+            dpg.add_selectable(label="Option 1", callback=dropdown_callback)
+            dpg.add_selectable(label="Option 2", callback=dropdown_callback)
+            dpg.add_selectable(label="Option 3", callback=dropdown_callback)
+
+        
+        
 
 
+
+    """ RUN GUI """
 
     dpg.setup_dearpygui()
     dpg.show_viewport()
